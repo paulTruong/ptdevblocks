9bfc5a5003dc3016b1f375d0e07fd0d1605a4489
Paul Truong <mrpaultruong@gmail.com>
2022-11-12 20:02:23 +1100

    Add prismjs
    Added prismjs, increased content width

--- functions.php
+++ functions.php
@@ -1,51 +1,76 @@
 <?php
 
-if ( ! function_exists( 'ptdevblocks_support' ) ) :
+if (!function_exists('ptdevblocks_support')) :
 
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * @since Twenty Twenty-Two 1.0
-	 *
-	 * @return void
-	 */
-	function ptdevblocks_support() {
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * @since Twenty Twenty-Two 1.0
+     *
+     * @return void
+     */
+    function ptdevblocks_support()
+    {
 
-		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
-
-	}
-
-endif;
-
-add_action( 'after_setup_theme', 'ptdevblocks_support' );
-
-if ( ! function_exists( 'ptdevblocks_styles' ) ) :
-
-	/**
-	 * Enqueue styles.
-	 *
-	 * @since Twenty Twenty-Two 1.0
-	 *
-	 * @return void
-	 */
-	function ptdevblocks_styles() {
-		// Register theme stylesheet.
-		$theme_version = wp_get_theme()->get( 'Version' );
-
-		$version_string = is_string( $theme_version ) ? $theme_version : false;
-		wp_register_style(
-			'ptdevblocks-style',
-			get_template_directory_uri() . '/style.css',
-			array(),
-			$version_string
-		);
-
-		// Enqueue theme stylesheet.
-		wp_enqueue_style( 'ptdevblocks-style' );
-
-	}
+        // Enqueue editor styles.
+        add_editor_style('style.css');
+    }
 
 endif;
 
-add_action( 'wp_enqueue_scripts', 'ptdevblocks_styles' );
+add_action('after_setup_theme', 'ptdevblocks_support');
+
+if (!function_exists('ptdevblocks_styles')) {
+
+    /**
+     * Enqueue styles.
+     *
+     * @since Twenty Twenty-Two 1.0
+     *
+     * @return void
+     */
+    function ptdevblocks_styles()
+    {
+        // Register theme stylesheet.
+        $theme_version = wp_get_theme()->get('Version');
+
+        $version_string = is_string($theme_version) ? $theme_version : false;
+        wp_register_style(
+            'ptdevblocks-style',
+            get_template_directory_uri() . '/style.css',
+            array(),
+            $version_string
+        );
+
+        // Enqueue theme stylesheet.
+        wp_enqueue_style('ptdevblocks-style');
+    }
+}
+add_action('wp_enqueue_scripts', 'ptdevblocks_styles');
+
+if (!function_exists('ptdevblocks_load_prismjs')) {
+
+    function ptdevblocks_load_prismjs()
+    {
+
+        if (has_block('core/code')) {
+
+            // Register prism.css
+            wp_register_style(
+                'prismcss',
+                get_template_directory_uri() . '/src/prism.css',
+            );
+
+            // Register prism.js
+            wp_register_script(
+                'prismjs', // handle name for the script
+                get_template_directory_uri() . '/src/prism.js' // location of the prism.js file
+            );
+
+            // Enqueue theme stylesheet.
+            wp_enqueue_style('prismcss');
+            wp_enqueue_script('prismjs');
+        }
+    }
+}
+add_action('wp_enqueue_scripts', 'ptdevblocks_load_prismjs');
