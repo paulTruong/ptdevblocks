4a289d8d334084c79b43b8d528adb53f2ade11b0
Paul Truong <paul@paultruong.dev>
2023-09-27 21:40:34 +1000

    fixed header template part

--- patterns/logotype.php
+++ patterns/logotype.php
@@ -9,5 +9,5 @@
  */
 ?>
 <!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"400","lineHeight":"0.9","letterSpacing":"0px"}},"textColor":"foreground","className":"logotype","fontSize":"large","fontFamily":"cartridge"} -->
-<p class="logotype has-foreground-color has-text-color has-cartridge-font-family has-large-font-size" style="font-style:normal;font-weight:400;letter-spacing:0px;line-height:0.9;text-transform:uppercase"><a href="http://paultruong.local/" data-type="page" data-id="9">Paul <br>Truong</a></p>
+<p class="logotype has-foreground-color has-text-color has-cartridge-font-family has-large-font-size" style="font-style:normal;font-weight:400;letter-spacing:0px;line-height:0.9;text-transform:uppercase"><a href="<?php echo home_url(); ?>" data-type="page" data-id="9">Paul <br>Truong</a></p>
 <!-- /wp:paragraph -->
